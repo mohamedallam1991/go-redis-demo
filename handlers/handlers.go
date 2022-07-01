659d402d82c098070554dff891063252a59cf442
Mohamed Allam <meda3gm@gmail.com>
2022-07-01 12:09:04 +0100

    fix

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -121,48 +121,48 @@ func (m *Repository) Try(w http.ResponseWriter, r *http.Request) {
 func connect() *redis.Client {
 	var opts *redis.Options
 
-	if os.Getenv("LOCAL") == "true" {
-		redisuri := os.Getenv("REDIS_URL")
-		fmt.Println("redisuri :", redisuri)
+	// if os.Getenv("LOCAL") == "true" {
+	// 	redisuri := os.Getenv("REDIS_URL")
+	// 	fmt.Println("redisuri :", redisuri)
 
-		redisAddress := fmt.Sprintf("%s:6379", redisuri)
-		fmt.Println("redisAddress :", redisAddress)
+	// 	redisAddress := fmt.Sprintf("%s:6379", redisuri)
+	// 	fmt.Println("redisAddress :", redisAddress)
 
-		// fmt.Println("Redis address :", redisAddress)
-		// fmt.Println("redis address :", redis:6379)
+	// 	// fmt.Println("Redis address :", redisAddress)
+	// 	// fmt.Println("redis address :", redis:6379)
 
-		opts = &redis.Options{
-			Addr:     redisAddress,
-			Password: "", // no password set
-			DB:       0,  // use default DB
-		}
-		fmt.Println("builtOpts :", opts)
+	// 	opts = &redis.Options{
+	// 		Addr:     redisAddress,
+	// 		Password: "", // no password set
+	// 		DB:       0,  // use default DB
+	// 	}
+	// 	fmt.Println("builtOpts :", opts)
 
-	} else {
-		// redisUri := `redis://:p5d63c80679f27374749b8fdde15820fb74f7da276e7c6eb5e5ac6dae4cfb61c3@ec2-100-26-75-186.compute-1.amazonaws.com:16529:6379`
-		// redisUri := `rediss://:p5d63c80679f27374749b8fdde15820fb74f7da276e7c6eb5e5ac6dae4cfb61c3@ec2-100-26-75-186.compute-1.amazonaws.com:16530`
-		redisUri := `redis://:p5d63c80679f27374749b8fdde15820fb74f7da276e7c6eb5e5ac6dae4cfb61c3@ec2-100-26-75-186.compute-1.amazonaws.com:16529`
-		// redis://:p5d63c80679f27374749b8fdde15820fb74f7da276e7c6eb5e5ac6dae4cfb61c3@ec2-100-26-75-186.compute-1.amazonaws.com:16529:6379:
-		// fmt.Println("redisUri :", redisUri)
-		// redisAddress := fmt.Sprintf("%s:6379", redisUri)
-		// fmt.Println("redisAddress :", redisAddress)
+	// } else {
+	// redisUri := `redis://:p5d63c80679f27374749b8fdde15820fb74f7da276e7c6eb5e5ac6dae4cfb61c3@ec2-100-26-75-186.compute-1.amazonaws.com:16529:6379`
+	// redisUri := `rediss://:p5d63c80679f27374749b8fdde15820fb74f7da276e7c6eb5e5ac6dae4cfb61c3@ec2-100-26-75-186.compute-1.amazonaws.com:16530`
+	redisUri := `redis://:p5d63c80679f27374749b8fdde15820fb74f7da276e7c6eb5e5ac6dae4cfb61c3@ec2-100-26-75-186.compute-1.amazonaws.com:16529`
+	// redis://:p5d63c80679f27374749b8fdde15820fb74f7da276e7c6eb5e5ac6dae4cfb61c3@ec2-100-26-75-186.compute-1.amazonaws.com:16529:6379:
+	// fmt.Println("redisUri :", redisUri)
+	// redisAddress := fmt.Sprintf("%s:6379", redisUri)
+	// fmt.Println("redisAddress :", redisAddress)
 
-		// redisAddress := fmt.Sprintf("%s:6379", redisUri)
-		// fmt.Println("redisAddress :", redisAddress)
-		//redis://<user>:<password>@<host>:<port>/<db_number>
+	// redisAddress := fmt.Sprintf("%s:6379", redisUri)
+	// fmt.Println("redisAddress :", redisAddress)
+	//redis://<user>:<password>@<host>:<port>/<db_number>
 
-		builtOpts, err := redis.ParseURL(redisUri)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("builtOpts :", opts)
-
-		// builtOpts, err := redis.ParseURL(os.Getenv("RZEDIS_URL"))
-		// if err != nil {
-		// 	panic(err)
-		// }
-		opts = builtOpts
+	builtOpts, err := redis.ParseURL(redisUri)
+	if err != nil {
+		panic(err)
 	}
+	fmt.Println("builtOpts :", opts)
+
+	// builtOpts, err := redis.ParseURL(os.Getenv("RZEDIS_URL"))
+	// if err != nil {
+	// 	panic(err)
+	// }
+	opts = builtOpts
+	// }
 
 	rdb := redis.NewClient(opts)
 	return rdb
